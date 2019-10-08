88a9819a1668ba3590a9dea5b812f6bb198acd19
JasonReed234 <52047762+JasonReed234@users.noreply.github.com>
2019-10-08 17:16:22 -0600

    Update urls.py

--- mormonwordsearch/urls.py
+++ mormonwordsearch/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from . import views
 
 urlpatterns = [
-    path('wordinput/', views.wordinput, name='wordinput'),    
-    path('your-word/', views.your_word, name='your_word')
+    path('search/', views.search, name='wordinput'),
+    path('results/', views.results, name='your_word'),
+    #path(r'^$', views.search)
 ]
